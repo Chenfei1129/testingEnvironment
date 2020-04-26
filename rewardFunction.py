c7a017b559e90986af4e2f50f03639894c624df1
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-04-25 21:42:46 -0700

    Delete the isInObstacle function
    Put it in transition function

--- rewardFunction.py
+++ rewardFunction.py
@@ -1,12 +1,4 @@
 import numpy as np
-
-class IsInObstacle():
-	def __init__(self, xObstacle, yObstacle):
-		self.xObstacle = xObstacle
-		self.yObstacle = yObstacle
-
-	def __call__(self, state):
-		return (state[0] >= self.xObstacle[0] and state[0] <= self.xObstacle[1] and state[1] >= self.yObstacle[0] and state[1] <= self.yObstacle[1])
 		
 class RewardFunctionCompete():
     def __init__(self, actionCost, isTerminal, obstaclePenalty, isInObstacle, terminalReward):
