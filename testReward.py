fc99a42ca3c25213bb2b81cf81ac7d1b42b058e2
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-04-25 22:29:06 -0700

    Update test according to new transition functions

--- testReward.py
+++ testReward.py
@@ -8,41 +8,37 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 
 # Local import
-from src.MDPChasing.rewardFunction import RewardFunctionCompete,IsInObstacle
+from src.MDPChasing.rewardFunction import RewardFunctionCompete
+from src.MDPChasing.envNoPhysics import IsInObstacle2, IsTerminal2
 
 @ddt
 class TestReward(unittest.TestCase):
 	def setUp(self):
 		self.xBoundary = [0,640]
 		self.yBoundary = [0,480]
-		self.xObstacle = [300,400]
-		self.yObstacle = [300,400]
+		self.Obstacle = [[[300,400], [300, 400]]]
 		self.actionCost = -1
 		self.obstaclePenalty = -5   
-		self.isInObstacle = IsInObstacle(self.xObstacle, self.yObstacle)
-		self.isTerminal = lambda state: state == [5,5]
-		self.terminalReward = 3
+		self.isInObstacle = IsInObstacle2(self.Obstacle)
+		self.minDistance = 10
+		self.TerminalPosition = [500, 500]
+		self.isTerminal = IsTerminal2(self.minDistance, self.TerminalPosition)
+		self.terminalReward = 2
+
 	@data(
-		([100,450],[1,1],-1),
-		([200,300],[21,21],-1),
-		([350,350],[10,0],-6),
-		([5,5],[0,10],2)
+		([100, 450], [0, 0], -1),
+		([350, 310], [1, 0], -6),
+		([490, 500], [1, 0], 1)
+
 	) 
 	@unpack
-	def testRewardFunctionCompete(self,state,action,result):
+	def testRewardFunctionCompete(self, state, action, result):
 		findReward = RewardFunctionCompete(self.actionCost, self.isTerminal, self.obstaclePenalty, self.isInObstacle, self.terminalReward)
 		checkReward = findReward(state,action)
-		
 		self.assertEqual(checkReward, result)
 
 
 
-	@data(([350,350],True),([300,300],True),([0,300],False),([500,350],False))
-	@unpack
-	def testInObstacle(self,state,groundTruth):
-		calculated=self.isInObstacle(state)
-		self.assertEqual(calculated,groundTruth)
-
-
 if __name__ == '__main__':
     unittest.main()
+
