752c9139de1967b0d6992be27b0f76bb8d118ed4
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-04-28 22:32:35 -0700

    Change reward function according to new transition function.

--- testingEnvironment---MDP/src/MDPChasing/rewardFunction.py
+++ testingEnvironment---MDP/src/MDPChasing/rewardFunction.py
@@ -1,18 +1,19 @@
+
 import numpy as np
 		
 class RewardFunctionCompete():
-    def __init__(self, actionCost, isTerminal, obstaclePenalty, isInObstacle, terminalReward):
-        self.actionCost = actionCost## one second how much 
-        self.isInObstacle = isInObstacle
+    def __init__(self, actionCost, isTerminal, obstaclePenalty, isInSwamp, terminalReward):
+        self.actionCost = actionCost ## how much cost one step
+        self.isInSwamp = isInSwamp
         self.isTerminal = isTerminal
-        self.obstaclePenalty = obstaclePenalty
+        self.swampPenalty = obstaclePenalty
         self.terminalReward = terminalReward
 
-    def __call__(self, state, action):
+    def __call__(self, state, action, agentID):
         reward = self.actionCost
-        if self.isInObstacle(state):
-            reward+=self.obstaclePenalty
-        if self.isTerminal(state):
+        if self.isInSwamp(state, agentID):
+            reward+=self.swampPenalty
+        if self.isTerminal(state, agentID):
             reward += self.terminalReward
         return reward
 
