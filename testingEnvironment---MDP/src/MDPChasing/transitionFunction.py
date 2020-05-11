d89a9013c4ecd93b5c493c446f76332270cb8938
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-05-10 22:37:05 -0700

    Update Transition
    Change the structure.

--- testingEnvironment---MDP/src/MDPChasing/transitionFunction.py
+++ testingEnvironment---MDP/src/MDPChasing/transitionFunction.py
@@ -1,3 +1,4 @@
+
 import numpy as np 
 import itertools as it
 
@@ -15,6 +16,7 @@ class Reset():
                      for _ in range(self.numOfAgnet)]
         return initState
 
+
 class FixedReset():
     def __init__(self, initPositionList):
         self.initPositionList = initPositionList
@@ -22,29 +24,55 @@ class FixedReset():
     def __call__(self, trialIndex):
         initState = self.initPositionList[trialIndex]
         return initState
+ 
 
-class TransitInSwampWorld():# generic pass a list of transition functions.
-    def __init__(self, stayInBoundaryByReflectVelocity, transitionWithNoise):
-        self.stayInBoundaryByReflectVelocity = stayInBoundaryByReflectVelocity
-        self.transitionWithNoise = transitionWithNoise
+class MultiAgentTransitionInGeneral():
+    def __init__(self, allTransitions):
+        self.allTransitions = allTransitions
+
+    def __call__(self, allStates, allActions):
+        allNewStates = [self.allTransitions[i](allStates, allActions[i]) for i in range(len(allTransitions))]
+
+
+class MultiAgentTransitionInSwampWorld():
+    def __init__(self, multiAgentTransitionInGeneral, terminalPosition):
+        self.multiAgentTransitionInGeneral = MultiAgentTransitionInGeneral
+        self.terminalPosition = terminalPosition
 
     def __call__(self, state, action):
-        newState = np.array(state) + np.array(action)        
-        checkedNewStateAndVelocities = [self.stayInBoundaryByReflectVelocity(
-            position, velocity) for position, velocity in zip(newState, action)]
-        newState, newAction = list(zip(*checkedNewStateAndVelocities))
-        finalNewState = [self.transitionWithNoise(singleState) for singleState in newState]
-        return finalNewState
+        allStates = [state, terminalPosition]
+        allActions = [action, [0,0]]
+        return self.multiAgentTransitionInGeneral(allStates, allActions)
+
+
+class SingleAgentTransitionInSwampWorld():
+    def __init__(self, transitionWithNoise, stayInBoundaryByReflectVelocity, isTerminal):
+        self.stayInBoundaryByReflectVelocity = stayInBoundaryByReflectVelocity
+        self.transitionWithNoise = transitionWithNoise
+        self.isTerminal = isTerminal
+
+    def __call__(self, state, action):
+        if self.isTerminal(state)==True:
+            newState = state
+        else:
+            newState = np.array(state) + np.array(action)
+            newStateCheckBoundary, newActionCheckBoundary = self.stayInBoundaryByReflectVelocity(newState, action)
+            newState = newStateCheckBoundary
+            newaction = newActionCheckBoundary 
+            finalNewState = self.transitionWithNoise(newState)
+            return finalNewState
+
 
 class TransitionWithNoise():
-    def __init__(self, standardDeviation):          
-        self.standardDeviation = standardDeviation
+    def __init__(self, state):          
+        self.state = state
 
-    def __call__(self, mu):
-        x = np.random.normal(mu[0], self.standardDeviation[0])
-        y = np.random.normal(mu[1], self.standardDeviation[1])
+    def __call__(self, noise):
+        x = np.random.normal(state[0], self.noise[0])
+        y = np.random.normal(state[1], self.noise[1])
         result = [x, y]
         return result
+
 
 class StayInBoundaryByReflectVelocity():
     def __init__(self, xBoundary, yBoundary):
@@ -86,36 +114,15 @@ class CheckBoundary():
 
 
 class IsInSwamp():
-    def __init__(self, swamp, agentID):
+    def __init__(self, swamp):
         self.swamp = swamp
-        self.agentID = agentID
 
     def __call__(self, state):
-        inOrNot = [ (state[self.agentID][0] >= xEachSwamp[0] and state[self.agentID][0] <= xEachSwamp[1] and state[self.agentID][1] >= yEachSwamp[0] and state[self.agentID][1] <= yEachSwamp[1])
+        inOrNot = [ (state[0] >= xEachSwamp[0] and state[0] <= xEachSwamp[1] and state[1] >= yEachSwamp[0] and state[1] <= yEachSwamp[1])
              for xEachSwamp, yEachSwamp in self.swamp]
         if True in inOrNot:
             return True
         else:
             return False
 
-class IsTerminal():
-    def __init__(self, minDistance, terminalPosition, agentID):
-        self.minDistance = minDistance
-        self.terminalPosition = terminalPosition
-        self.agentID = agentID
-
-    def __call__(self, state):       
-        L2Normdistance = np.array([np.linalg.norm(np.array(self.terminalPosition) - np.array(state[self.agentID]), ord=2)] )          
-        return (L2Normdistance <= self.minDistance)
-
-class IsTerminalAll():
-    def __init__(self, minDistance, terminalPosition, isTerminal):
-        self.minDistance = minDistance
-        self.terminalPosition = terminalPosition
-        self.isTerminal = isTerminal
-
-    def __call__(self, state):     
-        
-        isTerminalOrNot = [self.isTerminal(state) for agentID in range(len(state)-1)]
-        return (True in isTerminalOrNot)
 
