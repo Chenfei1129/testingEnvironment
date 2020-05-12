dd3f44e643e4e14fed01fabd263c02bd51ecf915
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-05-12 12:15:37 -0700

    Update rewardfunction for all agents.

--- testingEnvironment---MDP/src/MDPChasing/rewardFunction.py
+++ testingEnvironment---MDP/src/MDPChasing/rewardFunction.py
@@ -1,20 +1,28 @@
 
 import numpy as np
 		
-class RewardFunctionCompete():
-    def __init__(self, actionCost, isTerminal, obstaclePenalty, isInSwamp, terminalReward):
-        self.actionCost = actionCost ## how much cost one step
+class RewardFunctionSingleAgent():
+    def __init__(self, actionCost, terminalReward, swampPenalty, isTerminal, isInSwamp):
+        self.actionCost = actionCost
+        self.swampPenalty = swampPenalty
+        self.terminalReward = terminalReward
         self.isInSwamp = isInSwamp
         self.isTerminal = isTerminal
-        self.swampPenalty = obstaclePenalty
-        self.terminalReward = terminalReward
 
-    def __call__(self, state, action, agentID):
+
+    def __call__(self, state, action, newstate):
         reward = self.actionCost
-        if self.isInSwamp(state, agentID):
+        if self.isInSwamp(state)==True: 
             reward+=self.swampPenalty
-        if self.isTerminal(state, agentID):
+        if self.isTerminal(state)==True:
             reward += self.terminalReward
         return reward
 
+class RewardFunctionAllAgent():
+    def __init__(self, allRewardFunctions):
+        self.allRewardFunctions = allRewardFunctions
+
+    def __call__(self, allStates, allActions, allNewStates):
+        allReward = [self.allRewardFunctions[i](allStates[i], allActions[i], allNewStates[i]) for i in range(len(self.allRewardFunctions))]
+        return allReward
 
